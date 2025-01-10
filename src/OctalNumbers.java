22073b1747a2a334b3da8c8b036ebb7bd0ffd172
Seira-02 <Priyankachahar0@gmail.com>
2025-01-10 19:06:08 +0530

    Adding OctalNumbers Program

---
+++ src/OctalNumbers.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class OctalNumbers {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input your number here: ");
+        int num, d;
+        num = sc.nextInt();
+        boolean flag =true;
+        while(num>0){
+            d = num%10;
+            if(d>7){
+                flag = false;
+                break;
+            }
+            num = num/10;
+        }
+        if(flag){
+            System.out.println("This number is octal");
+        }else{
+            System.out.println("This is not an octal number");
+        }
+
+
+    }
+}
