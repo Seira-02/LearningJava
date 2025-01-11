5c57acdd04799a719493cd23c95e95945dd0321c
Seira-02 <Priyankachahar0@gmail.com>
2025-01-11 13:36:02 +0530

    Prime number checker

---
+++ src/PrimeNoChecker.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class PrimeNoChecker {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your number: ");
+        int num, i;
+        num = sc.nextInt();
+        boolean flag = true;
+        for(i=2; i< num; i++){
+            if(num%i == 0){
+                flag = false;
+                System.out.println("This is not a Prime Number");
+                break;
+            }
+            }
+        if(flag) {
+            System.out.println("This is a Prime Number");}
+        }
+    }
+
